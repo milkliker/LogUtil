cb049de012419db9d326cc16f8073f87d675e545
HanPei <tyhj5@qq.com>
2018-10-09 16:45:07 +0800

    降低compileSdkVersion

--- app/src/main/java/com/example/dhht/logutil/MainActivity.java
+++ app/src/main/java/com/example/dhht/logutil/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import log.LogUtils;
+import toast.ToastUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LogUtils.init(true,null);
+        LogUtils.init(true, new LogUtils.OnLogInfoListener() {
+            @Override
+            public void onLog(String logFrom, String logInfo) {
+                ToastUtil.toast(MainActivity.this,logInfo);
+            }
+        });
         LogUtils.logJson("{ \"code\": 200, \"msg\": \"ok\", \"data\": { \"version\": \"1.0\", \"url\": \"http://ozr6klu3a.bkt.clouddn.com/app_1516350756866\", \"info\": \"第一个版本\", \"imageUrl\": null, \"forced\": false } }");
-        LogUtils.log("哈哈哈哈");
     }
 }
