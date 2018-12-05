e98de301f462db2c0803531eceac2eac1f5d59c3
HanPei <tyhj5@qq.com>
2018-12-05 11:52:04 +0800

    -

---
+++ loglibaray/src/main/java/UtilManager.java
@@ -0,0 +1,21 @@
+import android.app.Application;
+
+import toast.ToastUtil;
+import util.ClipbordUtil;
+import util.InternetUtil;
+import util.ScreenUtil;
+import util.SharedPreferencesUtil;
+import util.VibrateUtil;
+
+public class UtilManager {
+
+    public static void initAll(Application application) {
+        ToastUtil.init(application);
+        ScreenUtil.init(application);
+        ClipbordUtil.init(application);
+        InternetUtil.init(application);
+        SharedPreferencesUtil.init(application);
+        VibrateUtil.init(application);
+    }
+    
+}
